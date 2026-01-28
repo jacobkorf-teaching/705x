8c1513aa0b0105965c57fb6bbfb43fc13cb40e00
jacobkorf-teaching <jacob.korf@janesville.k12.wi.us>
2026-01-28 08:22:07 -0600

    new for loop

--- prog705x.java
+++ prog705x.java
@@ -2,6 +2,8 @@ public class prog705x {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
         System.out.println("Hello, World!");
-        System.out.println("Hello, World!");
+        for(int x = 0; x < 5; x++) {
+            System.out.print(x);
+        }
     }
 }
