79aac55121ac365142dde5579a3d2c25054aeaf4
jacobkorf-teaching <jacob.korf@janesville.k12.wi.us>
2026-02-02 07:36:47 -0600

    commit

--- prog705x.java
+++ prog705x.java
@@ -1,9 +1,4 @@
 public class prog705x {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
-        System.out.println("Hello, World!");
-        for(int x = 0; x < 5; x++) {
-            System.out.print(x);
-        }
     }
 }
